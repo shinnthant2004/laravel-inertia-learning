3bb878b9ebf61ab90a1432a427cce961261251c7
shinthantequi@gmail.com <shinthantequi@gmail.com>
2022-05-01 05:22:32 +0630

    setup and props

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>min ga lar par  {{ name }}</h1>
+        <div>
+            <ul>
+                <li v-for="framework in frameworks" key="framework">{{ framework }}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+      props:['name','frameworks'],
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
